e0ff9222b5524f260f2c1257b622a28ed8cbab52
tyt <tyt@tyt-PC>
2018-02-03 04:59:26 -0300

    -corregido el cambio de escalas -agregado funcion coseno -agregado funcion cuadratica

--- public_html/main.js
+++ public_html/main.js
@@ -2,9 +2,12 @@
  * 
  * arreglado bug de funcion seno
  * 
+ * -corregido el cambio de escalas
+ * -agregado funcion coseno
+ * -agregado funcion cuadratica
  */
 
-var escala = 20;
+var escala = 20; //get del elemento e?
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var xf = c.getAttribute("width");
@@ -31,7 +34,7 @@ function graficarEje() {
     ctx.lineTo(xf / 2, yf);
     ctx.stroke();
 
-    for (var i = 1; i < xf / escala; i++) {
+    for (var i = 1; i < xf / escala * 2; i++) {
         //mostrarmensaje(i*escala);
         //alert(i * escalaX);
         //alert(y + 5);
@@ -40,14 +43,22 @@ function graficarEje() {
         //si no solo tira un alert y no hace nada mas
         //si se puedeeeeee!!!!
         ctx.beginPath();
-        ctx.moveTo(i * escala, yf / 2 + 10);
-        ctx.lineTo(i * escala, yf / 2 - 10);
+        ctx.moveTo(xf / 2 + i * escala, yf / 2 + 10);
+        ctx.lineTo(xf / 2 + i * escala, yf / 2 - 10);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(xf / 2 - i * escala, yf / 2 + 10);
+        ctx.lineTo(xf / 2 - i * escala, yf / 2 - 10);
         ctx.stroke();
 
-        if (i * escala < yf) {
+        if (i * escala < yf / 2) {
             ctx.beginPath();
-            ctx.moveTo(xf / 2 - 10, i * escala);
-            ctx.lineTo(xf / 2 + 10, i * escala);
+            ctx.moveTo(xf / 2 - 10, yf / 2 + i * escala);
+            ctx.lineTo(xf / 2 + 10, yf / 2 + i * escala);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(xf / 2 - 10, yf / 2 - i * escala);
+            ctx.lineTo(xf / 2 + 10, yf / 2 - i * escala);
             ctx.stroke();
             //ctx.save();
         }
@@ -56,7 +67,7 @@ function graficarEje() {
 }
 
 function setValoresSe(e, fr, am, fa) {
-    escala=Number(e);
+    escala = Number(e);
     m = Number(fr);
     a = Number(am);
     b = Number(fa);
@@ -64,6 +75,18 @@ function setValoresSe(e, fr, am, fa) {
     document.getElementById("a1").innerHTML = am;
     document.getElementById("fa1").innerHTML = fa;
     graficarFuncionSe();
+
+}
+
+function setValoresCo(e, fr, am, fa) {
+    escala = Number(e);
+    m = Number(fr);
+    a = Number(am);
+    b = Number(fa);
+    document.getElementById("fr1").innerHTML = fr;
+    document.getElementById("a1").innerHTML = am;
+    document.getElementById("fa1").innerHTML = fa;
+    graficarFuncionCo();
 
 }
 
@@ -83,18 +106,52 @@ function graficarFuncionSe() {
     //}, 300);
     ctx.strokeStyle = "blue";
     for (xS = 0; xS < xf; xS += escala / 5) {
-        
+
         // alert(yS);
-        yS = (yf / 2 - a * escala * Math.sin(b + m * ((xS / escala) 
-                 - ( xf / (escala * 2))
-                ) ));
+        yS = (yf / 2 - a * escala * Math.sin(b + m * ((xS / escala)
+                - (xf / (escala * 2))
+                )));
         //  alert(yS);
-       // mostrarmensaje(xS + " " + (b + m * ((xS / escala) 
-       //          - ( xf / (escala * 2))
-       //         )) );
+        // mostrarmensaje(xS + " " + (b + m * ((xS / escala) 
+        //          - ( xf / (escala * 2))
+        //         )) );
         ctx.beginPath();
         ctx.moveTo(xS - escala / 5, yA);
-        ctx.lineTo(xS , yS);
+        ctx.lineTo(xS, yS);
+        ctx.stroke();
+        yA = yS;
+
+    }
+}
+
+function graficarFuncionCo() {
+    //mostrarmensaje("entro a la funcion");
+    var xS = 1;
+    var yS = yf / 2;
+    var yA = yf / 2;
+
+
+
+    ctx.fillStyle = "coral";
+    ctx.fillRect(0, 0, 800, 600);
+    graficarEje();
+    //setInterval(function () {
+    //    trazarLineaSeno()
+    //}, 300);
+    ctx.strokeStyle = "blue";
+    for (xS = 0; xS < xf; xS += escala / 5) {
+
+        // alert(yS);
+        yS = (yf / 2 - a * escala * Math.cos(b + m * ((xS / escala)
+                - (xf / (escala * 2))
+                )));
+        //  alert(yS);
+        // mostrarmensaje(xS + " " + (b + m * ((xS / escala) 
+        //          - ( xf / (escala * 2))
+        //         )) );
+        ctx.beginPath();
+        ctx.moveTo(xS - escala / 5, yA);
+        ctx.lineTo(xS, yS);
         ctx.stroke();
         yA = yS;
 
@@ -112,15 +169,62 @@ function graficarFuncionSe() {
 //    yA = yS;
 //    xS += escala;
 //}
+function setValoresCu(e, fr, am, fa) {
+    escala = Number(e);
+    m = Number(fr);
+    a = Number(am);
+    b = Number(fa);
+    document.getElementById("a1").innerHTML = fr;
+    document.getElementById("b1").innerHTML = am;
+    document.getElementById("c1").innerHTML = fa;
+    graficarFuncionCu();
+
+}
+
+function graficarFuncionCu() {
+    var xS = 1;
+    var yS = yf / 2;
+    var yA = yf / 2;
+
+
+
+    ctx.fillStyle = "coral";
+    ctx.fillRect(0, 0, 800, 600);
+    graficarEje();
+    //setInterval(function () {
+    //    trazarLineaSeno()
+    //}, 300);
+    ctx.strokeStyle = "blue";
+    for (xS = 0; xS < xf; xS += escala / 5) {
+
+        // alert(yS);
+        //yS = (yf / 2 - a * escala * Math.cos(b + m * ((xS / escala)
+        //        - (xf / (escala * 2))
+        //        )));
+        var x = xS / escala - xf / (escala * 2);
+        yS = yf / 2 - escala * (m * x * x + a*x +b) ;
+        //  alert(yS);
+        // mostrarmensaje(xS + " " + (b + m * ((xS / escala) 
+        //          - ( xf / (escala * 2))
+        //         )) );
+        ctx.beginPath();
+        ctx.moveTo(xS - escala / 5, yA);
+        ctx.lineTo(xS, yS);
+        ctx.stroke();
+        yA = yS;
+
+    }
+}
+
 function setValoresLi(e, p, b) {
     //alert("entro a la funcion");
     //alert(p+ "   " + b);
     // document.getElementsByClassName("pendiente").innerHTML = p;
     // document.getElementsByClassName("altura").innerHTML = b;
-    escala=Number(e);
+    escala = Number(e);
     m = Number(p);
     a = Number(b);
-    
+
     //document.getElementById("pend").innerHTML = p;
     //document.getElementById("alt").innerHTML = b;
     document.getElementById("pend1").innerHTML = p;
@@ -152,13 +256,13 @@ function graficarFuncionLi() {
         yC += -(m * escala);
         //mostrarmensaje("x " + xC + "  y" +  yC);
     } while (xC > 0 && xC < xf && yC > 0 && yC < yf)
-    
+
 //} while (xC + escala > 0 && xC + escala <xf && yC -(m * escala) >= 0 && yC -(m * escala) <= yf)
     //mostrarmensaje("punto 2: x " + xC + "  y" +  yC);
     ctx.lineTo(xC, yC);
     ctx.strokeStyle = "blue";
     ctx.stroke();
- //mostrarmensaje(xC + "  " + yC);
+    //mostrarmensaje(xC + "  " + yC);
 }
 
 
